d4da0f1a74395b69729336c1c1d66f322be1b45e
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:02:42 -0600

    feat: add storage keys

---
+++ src/data/storageKeys.ts
@@ -0,0 +1,5 @@
+export const PRODUCTS_STORAGE_KEY = "facturacionv2_products_v1";
+export const EMPLOYEES_STORAGE_KEY = "facturacionv2_employees_v1";
+export const CLIENTS_STORAGE_KEY = "facturacionv2_clients_v1";
+export const CONTACTS_STORAGE_KEY = "facturacionv2_contacts_v1";
+export const QUOTES_STORAGE_KEY = "facturacionv2_quotes_v1";
