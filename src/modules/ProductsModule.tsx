f8a99cccaeb785bd4b642786e6a3de59a37df341
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:12:28 -0600

    feat: scaffold ProductsModule

---
+++ src/modules/ProductsModule.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EmptyModule } from "../components/EmptyModule";
+
+export default function ProductsModule() {
+  return (
+    <EmptyModule
+      title="Productos"
+      description="Aquí moveremos el catálogo de productos con costo, margen y precio de venta calculado."
+    />
+  );
+}
