b13a7f60e407b8d7646cba64cd636d1757f91572
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:13:03 -0600

    feat: scaffold ClientsModule

---
+++ src/modules/ClientsModule.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EmptyModule } from "../components/EmptyModule";
+
+export default function ClientsModule() {
+  return (
+    <EmptyModule
+      title="Clientes"
+      description="Aquí moveremos el CRUD completo de clientes."
+    />
+  );
+}
