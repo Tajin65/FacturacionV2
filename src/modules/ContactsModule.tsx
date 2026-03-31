48b88367591106f0103fb62e09efea8019a18933
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:13:21 -0600

    feat: scaffold ContactsModule

---
+++ src/modules/ContactsModule.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EmptyModule } from "../components/EmptyModule";
+
+export default function ContactsModule() {
+  return (
+    <EmptyModule
+      title="Contactos"
+      description="Aquí moveremos el CRUD de contactos asociados a clientes."
+    />
+  );
+}
