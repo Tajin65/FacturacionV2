8853210612e852eaa948083ada6b2f7dcca3cb06
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:06:50 -0600

    feat: scaffold QuotesModule

---
+++ src/modules/QuotesModule.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EmptyModule } from "../components/EmptyModule";
+
+export default function QuotesModule() {
+  return (
+    <EmptyModule
+      title="Cotizaciones"
+      description="Aquí construiremos el generador de cotizaciones con cliente, contacto, vendedor, partidas, moneda, tipo de cambio, subtotal, IVA y total."
+    />
+  );
+}
