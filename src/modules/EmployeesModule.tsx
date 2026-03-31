783409408d9d5ee3b1dc1d1d5562b605ff9f2cd5
Tajin65 <fllmchia5@gmail.com>
2026-03-30 18:12:50 -0600

    feat: scaffold EmployeesModule

---
+++ src/modules/EmployeesModule.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { EmptyModule } from "../components/EmptyModule";
+
+export default function EmployeesModule() {
+  return (
+    <EmptyModule
+      title="Empleados"
+      description="Aquí moveremos el CRUD de empleados y las firmas digitales."
+    />
+  );
+}
